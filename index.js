1bbad620f990fe19deaabf87223dedd1ee54c095
James Ross <james@jross.me>
2020-11-15 02:32:53 +0000

    chore: allow longer lines in commit messages

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 	rules: {
+		'body-max-line-length': [2, 'always', Infinity],
+		'footer-max-line-length': [2, 'always', Infinity],
 		'type-enum': [
 			2,
 			'always',
